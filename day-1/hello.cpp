f092a080614bb682332e99e0e159cba658246dd1
rudragondaliya <gondaliyarudra61@gmail.com>
2024-06-15 14:58:51 +0530

    done

---
+++ day-1/hello.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // cout << "hello world!" << endl;
+    // cout << "next line";
+
+    // this is a comment
+//    int a,b,c;
+//    short sa = 9;
+//    short Sa = 10;
+
+//    cout << Sa << endl;
+//       cout << sa << endl;
+
+
+// camelcase Notations
+
+// int marksInMaths = 86;
+// cout<<marksInMaths;
+
+// short a;
+// int const b = 89;
+// long c;
+// long long d;
+// float score = 34;
+// double long score2 = 44;
+
+// cout<<"the score is " <<score2 << endl;
+// cout<<"the score is "<<score << endl;
+// cout<<b;
+
+
+//   int a,b;
+//   cout<<"enter first number";
+//   cin>>a;
+
+//   cout<<"enter second number";
+//   cin>>b;
+
+//   cout<<"a+b is "<< a+b << endl; 
+//   cout<<"a-b is "<< a-b << endl;
+//   cout<<"a*b is "<< (double) a*b << endl;
+//   cout<<"a/b is "<< (float) a/b << endl;
+
+//   cout<<"the sum of a and b is "<< a+b;
+
+// int a,b,c;
+
+// cout<<"enter the first number";
+// cin>>a;
+
+// cout<<"enter the second number";
+// cin>>b;
+
+// cout<<"enter the third number";
+// cin>>c;
+
+// cout<<"the sum of three number is "<< a - b + c;
+
+//  int age;
+//  cout<<"enter your age"<< endl;
+//  cin>>age; 
+
+//  if(age >150){
+//     cout<<"invalid age";
+//  }
+//  else if (age >18){
+//     cout<<"you can vote";
+//  }
+
+ 
+//  else{
+//     cout<<"you cannot vote";
+//  }
+
+
+//   int index = 0;
+//   while(index <=34) {
+//     cout<<"we are at index number "<<index<<endl;
+//     index = index + 1;
+//   }
+
+
+// do
+// {
+//      cout<<"we are at index number "<<index<<endl;
+//     index = index + 1;
+    
+// } while (index< 33);
+
+
+// for (int i = 0; i < 34; i++)
+// {
+//     cout<<"the value of i is "<<i<<endl;
+//     }
+
+
+
+//     return 0;
+// }
+
+
